e34bc5b933394d5524f1d82eb5ce6394752412c6
J-Zach Loke <jzloke@gmail.com>
2020-02-23 20:30:48 -0800

    changed clip bank to be less inclusive, but much more space efficient

--- utilities.py
+++ utilities.py
@@ -70,16 +70,19 @@ def get_max_word_count() -> int:
 
 def generate_clip_bank():
     '''
-    generates a json file with combinations of every soundboard file name
-    clip_bank[3] is a dict with keys being combinations of 3 words of every soundboard file name and keys being the file name
+    generates a json file with all consecutive combinations of every soundboard file name
+    clip_bank[2] is a dict with keys being combinations of 3 consecutive words of every soundboard file name and keys being the file name
     ex: file name = 'you stole all my glory'
-    clip_bank[3] = {'you stole all': 'you stole all my glory.mp3',
-                    'you stole my': 'you stole all my glory.mp3'} etc
+    clip_bank[2] = {'you stole': 'you stole all my glory.mp3',
+                    'stole all': 'you stole all my glory.mp3',
+                    'all my': 'you stole all my gloyr.mp3'} etc
+                NOT 'you stole' and 'you all'
     '''
     # initialize the bank
     clip_bank = {}
     for i in range(1, get_max_word_count()+1):
         clip_bank[i] = {}
+    skip = []
 
     # populate
     for file_name in listdir('soundboard/'):
@@ -87,13 +90,14 @@ def generate_clip_bank():
         word_list = fixed_file_name.split(' ')
         word_count = len(word_list)
 
-        clip_bank[word_count][fixed_file_name] = file_name # combinations doesn't count the full string
-        for words in range(word_count):
-            combos = list(combinations(word_list, words))
-            for phrase_list in combos:
-                if phrase_list:
-                    phrase = ' '.join(phrase_list)
-                    clip_bank[words][phrase] = file_name
+        combos = [word_list[i:j] for i, j in combinations(range(word_count+1), 2)] # gets all consecutive combos
+        for phrase_list in combos:
+            phrase = ' '.join(phrase_list) # make sure to turn them back into strings and not [str]
+            if phrase in clip_bank[len(phrase_list)]: # ignore common word combinations like "all" or "i'm gonna"
+                del clip_bank[len(phrase_list)][phrase]
+                skip.append(phrase)
+            if phrase not in skip:
+                clip_bank[len(phrase_list)][phrase] = file_name
 
     with open('clip_bank.json', 'w+') as file:
         json.dump(clip_bank, file)
